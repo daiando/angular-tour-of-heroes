bbc4db313387f1de2f36cdce02e9c47cb1ea9486
DAI ANDO <daifloss@gmail.com>
2017-03-03 01:15:42 +0900

    learn auth routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CrisisListComponent } from './crisis-list.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+
+import { HeroesModule} from './heros/heroes.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
